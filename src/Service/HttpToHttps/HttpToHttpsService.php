ff47041a63f4c957d3a458a7bdde82404c9debf7
Alex <ajm.dosreis.daponte@gmail.com>
2022-04-29 19:36:59 +0200

    Added Conversion to HTTPS only in prod

--- src/Service/HttpToHttps/HttpToHttpsService.php
+++ src/Service/HttpToHttps/HttpToHttpsService.php
@@ -6,16 +6,16 @@ class HttpToHttpsService
 {
     private static string $needle = "http://";
     private static string $freshNeedle = "https://";
-    // private string $appEnv;
+    private string $appEnv;
 
-    // public function __construct(string $appEnv)
-    // {
-    //     $this->appEnv = $appEnv;
-    // }
+    public function __construct(string $appEnv)
+    {
+        $this->appEnv = $appEnv;
+    }
 
     public function convert(string $url): string
     {
-        if (str_starts_with($url, self::$needle)) {
+        if ($this->appEnv === 'prod' && str_starts_with($url, self::$needle)) {
             return str_replace(self::$needle, self::$freshNeedle, $url);
         }
         return $url;
